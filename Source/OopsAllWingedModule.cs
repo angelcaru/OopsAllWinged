11eb838903a4e039a418173bc6469c0b0c76fa1b
angelcaru <angelcaru10@gmail.com>
2025-12-13 00:22:26 +0100

    fix crash when loading moon berry

--- Source/OopsAllWingedModule.cs
+++ Source/OopsAllWingedModule.cs
@@ -19,6 +19,7 @@ public class OopsAllWingedModule : EverestModule {
 
     public static void OnBerryCtor(On.Celeste.Strawberry.orig_ctor orig, Strawberry self, EntityData data, Vector2 offset, EntityID gid) {
         data.Values["winged"] = true;
+        data.Values["moon"] = false;
         orig(self, data, offset, gid);
     }
 
